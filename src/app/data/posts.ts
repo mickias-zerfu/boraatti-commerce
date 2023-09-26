292cd2832ec70fd0c8da145d2d7bfef8e4b30e30
Mikiyas Zerfu <mickiaszerfu@gmail.com>
2023-09-26 09:10:25 +0300

    some update

--- src/app/data/posts.ts
+++ src/app/data/posts.ts
@@ -1,60 +1,55 @@
 import { BlogModel } from "../models/blog.model";
 
-  export const postsInfo: BlogModel[] = [
-      {
-          "id":1,
-          "title":"Welcome to the new blog",
-          "image_url":"https://via.placeholder.com/600/FF2921",
-          "content":"Book description: In June, 1954, eighteen-year-old Emmett Watson is driven home to Nebraska by the warden of the juvenile work farm where he has just served fifteen months for involuntary manslaughter. His mother long gone, his father recently deceased, and the family farm foreclosed upon by the bank, Emmett's intention is to pick up his eight-year-old brother, Billy, and head to California where they can start their lives anew. But when the warden drives away, Emmett discovers that two friends from the work farm have hidden themselves in the trunk of the warden's car. Together, they have hatched an altogether different plan for Emmett's future, one that will take them all on a fateful journey in the opposite direction—to the City of New York.",
-          "created_at":"12/2/2023",
-          "updated_at":"12/3/2023"
-      },
-      {
-        "id": 2,
-        "title": "Constantine Foundation Launches New Charity Initiative",
-        "image_url": "https://via.placeholder.com/600/5CE41E",
-        "content": "The Constantine Foundation is excited to announce the launch of its new charity initiative, the Constantine Cares Fund. The fund will support a variety of charitable causes, with a focus on helping children and families in need The Constantine Cares Fund will provide grants to organizations that are working to make a difference in the lives of children and families. Grants will be awarded to organizations that are providing essential services, such as food, shelter, and medical care. The fund will also support organizations that are providing educational opportunities and other programs that help children and families reach their full potential.      The Constantine Cares Fund is a way for the Constantine Foundation to give back to the community and make a difference in the lives of those who need it most. We are committed to supporting organizations that are making a real impact in the lives of children and families, and we hope that you will join us in supporting the Constantine Cares Fund. To learn more about the Constantine Cares Fund, please visit our website at www.constantinefoundation.org/cares. You can also make a donation to the fund by visiting our website or by mailing a check to the following address: Constantine Foundation",
-        "created_at": "12/2/2023",
-        "updated_at": "12/3/2023"
-      },
-      {
-        "id": 3, "title": "Constantine Foundation Partners with Local Food Bank",
-        "image_url": "https://via.placeholder.com/600/FFA73D",
-        "content": "The Constantine Foundation is proud to announce its partnership with the local food bank. The foundation has donated \$100,000 to the food bank to help provide food to families in need.The food bank is a vital resource for families in our community who are struggling to make ends meet. The donation from the Constantine Foundation will help the food bank provide food to thousands of families each year.The Constantine Foundation is committed to helping those in need in our community. This partnership with the food bank is just one way that we are working to make a difference in the lives of our neighbors.If you would like to learn more about the Constantine Foundation or the food bank, please visit our websites at www.constantinefoundation.org and www.foodbank.org. You can also make a donation to the food bank by visiting their website or by mailing a check to the following address:Local Food Bank Attn: Donations 123 Main Street Anytown, CA 91234 Thank you for your support of the Constantine Foundation and the local food bank!",
-        "created_at": "12/2/2023",
-        "updated_at": "12/3/2023"
-      },
-      {
-        "id": 4,
-        "title": "Con stantine Foundation Supports Local Children's Hospital",
-        "image_url": "https://via.placeholder.com/600/FF2921",
-        "content": "The Constantine Foundation is proud to announce its support of the local children's hospital. The foundation has donated \$500,000 to the hospital to help provide care to sick children in our community. The children's hospital is a vital resource for families in our community who have children with serious illnesses. The donation from the Constantine Foundation will help the hospital provide care to thousands of children each year. The Constantine Foundation is committed to helping those in need in our community. This partnership with the children's hospital is just one way that we are working to make a difference in the lives of our neighbors. If you would like to learn more about the Constantine Foundation or the children's hospital, please visit our websites at www.constantinefoundation.org and www.childrenshospital.org. You can also make a donation to the children's hospital by visiting their website or by mailing a check to the following address: Local Children's Hospital Attn: Donations 123 Main Street Anytown, CA 91234 Thank you for your support of the ",
-        "created_at": "12/2/2023",
-        "updated_at": "12/3/2023"
-      },
-      {
-          "id":5,
-          "title":"How to be a good programmer",
-          "image_url":"https://via.placeholder.com/600/9ff952",
-          "content":"Book description: In June, 1954, eighteen-year-old Emmett Watson is driven home to Nebraska by the warden of the juvenile work farm where he has just served fifteen months for involuntary manslaughter. His mother long gone, his father recently deceased, and the family farm foreclosed upon by the bank, Emmett's intention is to pick up his eight-year-old brother, Billy, and head to California where they can start their lives anew. But when the warden drives away, Emmett discovers that two friends from the work farm have hidden themselves in the trunk of the warden's car. Together, they have hatched an altogether different plan for Emmett's future, one that will take them all on a fateful journey in the opposite direction—to the City of New York.",
-          "created_at":"12/2/2022",
-          "updated_at":"12/3/2023"
-      },
-      {
-          "id":6,
-          "title":"How to delete a post",
-          "image_url":"https://via.placeholder.com/600/920002",
-          "content":"Book description: In June, 1954, eighteen-year-old Emmett Watson is driven home to Nebraska by the warden of the juvenile work farm where he has just served fifteen months for involuntary manslaughter. His mother long gone, his father recently deceased, and the family farm foreclosed upon by the bank, Emmett's intention is to pick up his eight-year-old brother, Billy, and head to California where they can start their lives anew. But when the warden drives away, Emmett discovers that two friends from the work farm have hidden themselves in the trunk of the warden's car. Together, they have hatched an altogether different plan for Emmett's future, one that will take them all on a fateful journey in the opposite direction—to the City of New York.",
-          "created_at":"12/2/2023",
-          "updated_at":"12/3/2023"
-      },
-      {
-          "id":7,
-          "title":"This is the end of the world",
-          "image_url":"https://via.placeholder.com/600/122252",
-          "content":"Book description: In June, 1954, eighteen-year-old Emmett Watson is driven home to Nebraska by the warden of the juvenile work farm where he has just served fifteen months for involuntary manslaughter. His mother long gone, his father recently deceased, and the family farm foreclosed upon by the bank, Emmett's intention is to pick up his eight-year-old brother, Billy, and head to California where they can start their lives anew. But when the warden drives away, Emmett discovers that two friends from the work farm have hidden themselves in the trunk of the warden's car. Together, they have hatched an altogether different plan for Emmett's future, one that will take them all on a fateful journey in the opposite direction—to the City of New York.",
-          "created_at":"12/2/2021",
-          "updated_at":"12/3/2023"
-      }
+export const postsInfo: BlogModel[] =
+
+  [
+    {
+      "id": 1,
+      "title": "Preserving Oromo Craftsmanship: Honoring Tradition and Identity",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Discover the rich heritage of Oromo cultural gifts and crafts. From beautifully handwoven textiles to intricate woodcarvings, this blog explores the significance of these crafts in preserving Oromo tradition and identity.",
+      "created_at": "2023-09-27",
+      "updated_at": "2023-09-27"
+    },
+    {
+      "id": 2,
+      "title": "The Art of Oromo Beadwork: A Colorful Tapestry of Oromo Culture",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Dive into the vibrant world of Oromo beadwork, where every bead tells a story. Learn about the symbolism behind each design and how this unique craft form is an integral part of Oromo cultural celebrations.",
+      "created_at": "2023-09-28",
+      "updated_at": "2023-09-28"
+    },
+    {
+      "id": 3,
+      "title": "Oromo Pottery: A Fusion of Art and Functionality",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Explore the ancient art of Oromo pottery and its significance in everyday life. From traditional cooking vessels to decorative pottery, this blog uncovers the techniques and cultural importance of Oromo pottery craftsmanship.",
+      "created_at": "2023-09-29",
+      "updated_at": "2023-09-29"
+    },
+    {
+      "id": 4,
+      "title": "The Beauty of Oromo Basketry: Woven Tales of Skill and Creativity",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Uncover the intricate world of Oromo basketry, where skilled weavers create stunning baskets using natural materials. Learn about the diverse types of baskets and their role in Oromo cultural practices and ceremonies.",
+      "created_at": "2023-09-30",
+      "updated_at": "2023-09-30"
+    },
+    {
+      "id": 5,
+      "title": "Oromo Leatherwork: A Tradition of Craftsmanship and Artistry",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Discover the mastery of Oromo leatherwork, from intricate designs adorning traditional garments to beautifully crafted accessories. This blog highlights the techniques, symbolism, and cultural significance behind Oromo leather craftsmanship.",
+      "created_at": "2023-10-01",
+      "updated_at": "2023-10-01"
+    },
+    {
+      "id": 6,
+      "title": "Traditional Oromo Jewelry: Adornments of Elegance and Cultural Heritage",
+      "image_url": "https://via.placeholder.com/600/FF2921",
+      "content": "Step into the world of Oromo jewelry, where timeless pieces reflect the grace and heritage of the Oromo people. Learn about the materials, craftsmanship, and symbolic meanings behind these exquisite adornments.",
+      "created_at": "2023-10-02",
+      "updated_at": "2023-10-02"
+    }
   ]
 
